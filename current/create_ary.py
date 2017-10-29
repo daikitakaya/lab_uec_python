9c9b6bc08f00f6d40fa8a2d8385bec0564a73695
Daiki Takaya <deiki08@gmail.com>
2017-10-29 20:54:38 +0900

    パラメーターの調整

--- current/create_ary.py
+++ current/create_ary.py
@@ -7,14 +7,16 @@ from numba.decorators import jit
 
 
 # -- ファイル書き込み設定 --
-v_output = open('neuron_V0.txt', 'w', encoding='utf8')
-raster_output = open('raster00.txt', 'w', encoding='utf8')
+v_output = open('neuron_V01.txt', 'w', encoding='utf8')
+I_output01 = open('neuron_I01.txt', 'w', encoding='utf8')
+I_output05 = open('neuron_I05.txt', 'w', encoding='utf8')
+raster_output = open('raster01.txt', 'w', encoding='utf8')
 sum_input_output01 = open('sum_input01.txt', 'w', encoding='utf8')
 sum_input_output05 = open('sum_input05.txt', 'w', encoding='utf8')
 
 # -- 各種パラメーター --
 TAU = 20.0
-W0 = 5.0
+W0 = 0.0
 V_th = 10.0
 V_max = 30.0
 V_re = 0.0
@@ -55,6 +57,7 @@ for t in times:
   I = np.zeros(NEURON_NUM) + 10.0 + np.random.uniform(-5.0,5.0,NEURON_NUM) #t秒における60個のニューロンへの入力(配列)
   sum_input = np.zeros(NEURON_NUM)
   for num_i in range(NEURON_NUM):#ニューロンごとの処理
+    sum_input[num_i] = sum_input[num_i] * np.exp(- (t - firing_times[num_i]) / 30.0)
     if num_i == 0:
       v_output.write(str(t) + "\t" + str(V[0]) + "\n")
     for connect_num in [i for i,j in enumerate(pair_ary[num_i,:]) if j == 1]: #接続している他ニューロンでループを回す
@@ -62,11 +65,13 @@ for t in times:
         s_ij[num_i][connect_num] += 1.0 #i番目のニューロンへの入力を1mV増加
         # s_ij[num_i][connect_num] = s_ij[num_i][connect_num] * np.exp(- (t - firing_times[num_i]) / 30.0)
         sum_input[num_i] += W0 * s_ij[num_i][connect_num] #接続しているニューロンからの入力の総和
-        sum_input[num_i] = sum_input[num_i] * np.exp(- (t - firing_times[num_i]) / 30.0)
+        # sum_input[num_i] = sum_input[num_i] * np.exp(- (t - firing_times[num_i]) / 30.0)
         if num_i == 0:
           sum_input_output01.write(str(t) + "\t" + str(sum_input[num_i]) + "\n")
+          I_output01.write(str(t) + "\t" + str(sum_input[num_i] + I[num_i]) + "\n")
         if num_i == 5:
           sum_input_output05.write(str(t) + "\t" + str(sum_input[num_i]) + "\n")
+          I_output05.write(str(t) + "\t" + str(sum_input[num_i] + I[num_i]) + "\n")
   for i in [i for i,j in enumerate(V) if j > V_th]:#15mV越える時
     t_sum += t - firing_times[i]
     t_interval.append(t - firing_times[i])
@@ -84,6 +89,8 @@ v_output.close()
 raster_output.close()
 sum_input_output01.close()
 sum_input_output05.close()
+I_output01.close()
+I_output05.close()
 t_interval_ave = t_sum / len(t_interval)
 count = 0
 for time in t_interval:
