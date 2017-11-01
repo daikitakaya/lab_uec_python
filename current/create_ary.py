f465646d29e2c5beceb9524d2d4f6b99a7aa2d82
Daiki Takaya <deiki08@gmail.com>
2017-11-01 18:57:30 +0900

    パラメータの調整

--- current/create_ary.py
+++ current/create_ary.py
@@ -2,21 +2,19 @@
 import numpy as np
 import math
 import networkx as nx
-import pandas as pd
 from numba.decorators import jit
 
 
 # -- ファイル書き込み設定 --
-v_output = open('neuron_V01.txt', 'w', encoding='utf8')
-I_output01 = open('neuron_I01.txt', 'w', encoding='utf8')
-I_output05 = open('neuron_I05.txt', 'w', encoding='utf8')
-raster_output = open('raster01.txt', 'w', encoding='utf8')
-sum_input_output01 = open('sum_input01.txt', 'w', encoding='utf8')
-sum_input_output05 = open('sum_input05.txt', 'w', encoding='utf8')
+v_output = open('neuron_V.txt', 'w', encoding='utf8')
+I_output01 = open('neuron_I.txt', 'w', encoding='utf8')
+raster_output01 = open('raster.txt', 'w', encoding='utf8')
+sum_input_output01 = open('sum_input.txt', 'w', encoding='utf8')
+exp_value = open('exp.txt', 'w', encoding='utf-8')
 
 # -- 各種パラメーター --
 TAU = 20.0
-W0 = 50.0
+W0 = 10.0
 V_th = 10.0
 V_max = 30.0
 V_re = 0.0
@@ -30,15 +28,19 @@ t2_sum = 0.0
 # -- カウント変数 --
 firing_count = 0
 
-G = nx.watts_strogatz_graph(n=NEURON_NUM,k=4,p=0.05) #スモールワールド作成
-pair_ary = np.asarray(nx.to_numpy_matrix(G)) #グラフをArrayに変換
+# G = nx.watts_strogatz_graph(n=NEURON_NUM,k=10,p=0.00) #スモールワールド作成
+# pair_ary = np.asarray(nx.to_numpy_matrix(G)) #グラフをArrayに変換
+pair_ary = np.ones([NEURON_NUM,NEURON_NUM])
 I = np.zeros([NEURON_NUM]) #入力の配列
-V = np.zeros([NEURON_NUM]) + np.random.uniform(-6,6,NEURON_NUM)
-sum_input = np.zeros([NEURON_NUM]) #各ニューロンへの入力の総和
+V = np.zeros([NEURON_NUM]) + np.random.uniform(-5,5,NEURON_NUM)
+S = np.zeros([NEURON_NUM]) #各ニューロンへの入力の総和
 s_ij = np.zeros([NEURON_NUM,NEURON_NUM]) #j番目からi番目のニューロンへの入力
-firing_times = np.zeros([NEURON_NUM])
-times = np.arange(0,1000,0.01)
-tt = np.zeros([NEURON_NUM,NEURON_NUM])
+firing_times = np.zeros([NEURON_NUM])# 発火時間を保持する配列
+times = np.arange(0,1000.01,0.01)# 時間を規定する配列
+time_interval = np.zeros([NEURON_NUM])
+
+for i in range(NEURON_NUM):
+  pair_ary[i][i] = 0.0
 
 @jit
 def lif(v, i, sum_i):
@@ -54,43 +56,37 @@ def runge(V,I,sum_input):
   return V + (v_k1 + (2 * v_k2) + (2 * v_k3) + v_k4) / 6.0
 
 for t in times:
-  I = np.zeros(NEURON_NUM) + 10.0 + np.random.uniform(-6.0,6.0,NEURON_NUM) #t秒における60個のニューロンへの入力(配列)
-  sum_input = np.zeros(NEURON_NUM)
+  I = np.zeros(NEURON_NUM) + 12.0 + np.random.uniform(-5.0,5.0,NEURON_NUM) #t秒における60個のニューロンへの入力(配列)
+  sum_input_output01.write(str(t) + "\t" + str(S[0]) + "\n")
+  v_output.write(str(t) + "\t" + str(V[0]) + "\n")
   for num_i in range(NEURON_NUM):#ニューロンごとの処理
-    sum_input[num_i] = sum_input[num_i] * np.exp(- (t - firing_times[num_i]) / 30.0)
+    for connect_num in [i for i,j in enumerate(pair_ary[num_i,:]) if j == 1.0]: #接続している他ニューロンでループを回す(), connect_numはnum_iと接続しているニューロン
+      if V[connect_num] >= V_th: #接続しているニューロン(j番目)が発火して入れば
+        s_ij[connect_num][num_i] += 1.0 #i番目のニューロンへの入力を1mV増加
+        S[num_i] += W0 * s_ij[connect_num][num_i] #接続しているニューロンからの入力の総和
+    S[num_i] = S[num_i] * math.exp(-(t - firing_times[num_i]) / 30.0)# 他ニューロンからの入力の総和の減衰
+  for num_i in [i for i,j in enumerate(V) if j >= V_th]:#15mV越える時
+    t_sum += t - firing_times[num_i]# 前回発火からの時間差を足し合わせる
+    t_interval.append(t - firing_times[num_i])# 時間差を記録
+    raster_output01.write(str(t) + "\t" + str(num_i + 1) + "\n") #ラスター出力
+    firing_times[num_i] = t # 発火時間を更新
+    V[num_i] = V_max #発火処理
     if num_i == 0:
-      v_output.write(str(t) + "\t" + str(V[0]) + "\n")
-    for connect_num in [i for i,j in enumerate(pair_ary[num_i,:]) if j == 1]: #接続している他ニューロンでループを回す
-      if V[connect_num] > V_th: #接続しているニューロン(j番目)が発火して入れば
-        s_ij[num_i][connect_num] += 1.0 #i番目のニューロンへの入力を1mV増加
-        # s_ij[num_i][connect_num] = s_ij[num_i][connect_num] * np.exp(- (t - firing_times[num_i]) / 30.0)
-        sum_input[num_i] += W0 * s_ij[num_i][connect_num] #接続しているニューロンからの入力の総和
-        # sum_input[num_i] = sum_input[num_i] * np.exp(- (t - firing_times[num_i]) / 30.0)
-        if num_i == 0:
-          sum_input_output01.write(str(t) + "\t" + str(sum_input[num_i]) + "\n")
-          I_output01.write(str(t) + "\t" + str(sum_input[num_i] + I[num_i]) + "\n")
-        if num_i == 5:
-          sum_input_output05.write(str(t) + "\t" + str(sum_input[num_i]) + "\n")
-          I_output05.write(str(t) + "\t" + str(sum_input[num_i] + I[num_i]) + "\n")
-  for i in [i for i,j in enumerate(V) if j > V_th]:#15mV越える時
-    t_sum += t - firing_times[i]
-    t_interval.append(t - firing_times[i])
-    raster_output.write(str(t) + "\t" + str(i + 1) + "\n") #ラスター出力
-    firing_times[i] = t
-    V[i] = V_max
-    if i == 0:
       firing_count += 1
       v_output.write(str(t) + "\t" + str(V[0]) + "\n")
-    V[i] = V_re
-  for i in [i for i,j in enumerate(V) if j < V_th]:#ルンゲで値更新
-    V[i] = runge(V[i], I[i], sum_input[i])
+    V[num_i] = V_re
+  for num_i in [i for i,j in enumerate(V) if j < V_th]:#ルンゲで値更新
+    V[num_i] = runge(V[num_i], I[num_i], S[num_i])
 
+
+# -- ファイル閉じる --
 v_output.close()
-raster_output.close()
+raster_output01.close()
 sum_input_output01.close()
-sum_input_output05.close()
 I_output01.close()
-I_output05.close()
+exp_value.close()
+
+# -- Rの計算 --
 t_interval_ave = t_sum / len(t_interval)
 count = 0
 for time in t_interval:
